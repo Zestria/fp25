0067f3f532e19de9b56170493f18b90241790a3b
Sterkachov <anymeshnik2.1@gmail.com>
2025-12-20 22:35:57 +0300

    feat: add abstract syntax tree

--- Davletshin/lib/ast.mli
+++ Davletshin/lib/ast.mli
@@ -11,9 +11,15 @@ type name = string
 (** The main type for our AST (дерева абстрактного синтаксиса) *)
 type 'name t =
   | Var of 'name (** Variable [x] *)
-  | Abs of 'name * 'name t (** Abstraction [λx.t] *)
-  | App of 'name t * 'name t
+  | Abs of 'name * 'name t (** Abstraction [fun x -> t] *)
+  | App of 'name t * 'name t (** Application [f g] *)
+  | Int of int (** Integer constant *)
+  | Add of 'name t * 'name t (** Addition [x + y] *)
+  | Sub of 'name t * 'name t (** Substraction [x - y] *)
+  | Mul of 'name t * 'name t (** Multiplication [x * y] *)
+  | Div of 'name t * 'name t (** Devision [x / y] *)
+  | Let of 'name * 'name t * 'name t (** Non-recursive let-binding [let x = y in z] *)
+  | If of 'name t * 'name t * 'name t (** Condition [if x then y else z] *)
 
-(* Application [f g ] *)
 (** In type definition above the 3rd constructor is intentionally without documentation
     to test linter *)
